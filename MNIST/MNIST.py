ab022bda367e2be07a1af8cc86ddf72c4b04b1e2
Jihee Ryu <chrisjihee@etri.re.kr>
2020-10-26 19:39:05 +0900

    Add MNIST.py: pytorch_lightning first example!

---
+++ MNIST/MNIST.py
@@ -0,0 +1,64 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torchvision.datasets import MNIST
+from torchvision import transforms
+import pytorch_lightning as pl
+import time
+
+
+class LitAutoEncoder(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28 * 28, 64),
+            nn.ReLU(),
+            nn.Linear(64, 3))
+        self.decoder = nn.Sequential(
+            nn.Linear(3, 64),
+            nn.ReLU(),
+            nn.Linear(64, 28 * 28))
+
+    def forward(self, x):
+        embedding = self.encoder(x)
+        return embedding
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+    def training_step(self, train_batch, batch_idx):
+        x, y = train_batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = F.mse_loss(x_hat, x)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = F.mse_loss(x_hat, x)
+        self.log('val_loss', loss)
+
+
+# data
+dataset = MNIST('../dat/', train=True, download=True, transform=transforms.ToTensor())
+mnist_train, mnist_val = random_split(dataset, [55000, 5000])
+
+train_loader = DataLoader(mnist_train, batch_size=32, num_workers=8)
+val_loader = DataLoader(mnist_val, batch_size=32, num_workers=8)
+
+# model
+model = LitAutoEncoder()
+
+# training
+t0 = time.time()
+trainer = pl.Trainer(gpus=2, accelerator="ddp", max_epochs=3, progress_bar_refresh_rate=20)
+trainer.fit(model, train_loader, val_loader)
+print(f"td={time.time() - t0:.3f}")

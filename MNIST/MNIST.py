9ee402276f35eec060b78c01d3057a09ad72045a
Jihee Ryu <chrisjihee@etri.re.kr>
2020-10-29 00:55:05 +0900

    Add MNIST2.py

--- MNIST/MNIST.py
+++ MNIST/MNIST.py
@@ -1,10 +1,11 @@
 import time
+
+import pytorch_lightning as pl
 import torch
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import MNIST
 from torchvision import transforms
-import pytorch_lightning as pl
+from torchvision.datasets import MNIST
 
 
 def show_accuracy(name, acc):
---
+++ MNIST/MNIST2.py
@@ -0,0 +1,113 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
+from pytorch_lightning.metrics.functional import accuracy
+from torch import nn
+from torch.utils.data import random_split, DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+
+def show_accuracy(name, acc):
+    detail = f'(={acc.correct}/{acc.total})'
+    metric = acc.compute()
+    return f'{name} Accuracy: {metric * 100:.2f}% {detail}'
+
+
+torch.manual_seed(777)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(777)
+
+
+class MNISTDataModule(pl.LightningDataModule):
+    def __init__(self, data_dir='/dat/data/'):
+        super().__init__()
+        self.data_dir = data_dir
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+
+        # self.dims is returned when you call dm.size()
+        # Setting default dims here because we know them.
+        # Could optionally be assigned dynamically in dm.setup()
+        self.dims = (1, 28, 28)
+        self.num_classes = 10
+
+    def prepare_data(self):
+        # download
+        MNIST(self.data_dir, train=True, download=True)
+        MNIST(self.data_dir, train=False, download=True)
+
+    def setup(self, stage=None):
+        # Assign train/val datasets for use in dataloaders
+        if stage == 'fit' or stage is None:
+            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
+            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+        # Assign test dataset for use in dataloader(s)
+        if stage == 'test' or stage is None:
+            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=32)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=32)
+
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test, batch_size=32)
+
+
+class LitModel(pl.LightningModule):
+    def __init__(self, channels, width, height, num_classes, hidden_size=64, learning_rate=2e-4):
+        super().__init__()
+
+        # We take in input dimensions as parameters and use those to dynamically build model.
+        self.channels = channels
+        self.width = width
+        self.height = height
+        self.num_classes = num_classes
+        self.hidden_size = hidden_size
+        self.learning_rate = learning_rate
+
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(channels * width * height, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.model(x)
+        return F.log_softmax(x, dim=1)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        acc = accuracy(preds, y)
+        self.log('val_loss', loss, prog_bar=True)
+        self.log('val_acc', acc, prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+
+
+if __name__ == '__main__':
+    dm = MNISTDataModule()
+    model = LitModel(*dm.size(), dm.num_classes)
+    trainer = pl.Trainer(max_epochs=3, progress_bar_refresh_rate=20, gpus=1)
+    trainer.fit(model, dm)
